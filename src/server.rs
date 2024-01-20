59db858c7254a250cab89fc2dddb54a7672b942e
Tom Anderson <tom@ando.gq>
2024-01-20 19:40:28 +1100

    use proper router and signature generation for server

--- src/server.rs
+++ src/server.rs
@@ -1,18 +1,24 @@
-use std::collections::BTreeMap;
+use std::rc::Rc;
 
+use matchit::Router;
 use serde_json::Value;
 use ts_rs::TS;
 
 use crate::handler::{ErasedHandler, Handler, MakeErasedHandler};
 
 pub struct Server {
-    routes: BTreeMap<String, Box<dyn ErasedHandler>>,
+    /// Keep all handlers in a Vec in-order to infer signatuers for server
+    signature_routes: Vec<(String, Rc<Box<dyn ErasedHandler>>)>,
+
+    /// Only allow router to store references to handlers
+    router: Router<Rc<Box<dyn ErasedHandler>>>,
 }
 
 impl Server {
     pub fn new() -> Self {
         Self {
-            routes: BTreeMap::new(),
+            signature_routes: Vec::new(),
+            router: Router::new(),
         }
     }
 
@@ -25,34 +31,40 @@ impl Server {
         Params: TS,
         Return: TS,
     {
-        self.routes.insert(
-            route.as_ref().to_string(),
-            Box::new(MakeErasedHandler {
-                handler,
-                do_call: |handler, params| handler.call(params),
-                get_signature: |handler| (handler.get_parameter_types(), handler.get_return_type()),
-            }),
-        );
+        let handler = Rc::new(Box::new(MakeErasedHandler {
+            handler,
+            do_call: |handler, params| handler.call(params),
+            get_signature: |handler| (handler.get_parameter_types(), handler.get_return_type()),
+        }) as Box<dyn ErasedHandler>);
+
+        self.signature_routes
+            .push((route.as_ref().to_string(), handler.clone()));
+
+        self.router
+            .insert(route.as_ref().to_string(), handler)
+            .unwrap();
 
         self
     }
 
     pub fn call(&self, route: impl AsRef<str>, parameters: Value) -> Value {
-        self.routes
-            .get(route.as_ref())
+        self.router
+            .at(route.as_ref())
             .unwrap()
+            .value
             .clone_box()
             .call(parameters)
     }
 
-    pub fn get_signatures(&self) -> Vec<String> {
-        self.routes
+    pub fn get_signatures(&self) -> String {
+        let body = self
+            .signature_routes
             .iter()
             .map(|(route, handler)| {
                 let (parameter_types, return_type) = handler.get_signature();
 
                 let signature = format!(
-                    "const {} = ({}) => {return_type};",
+                    "{}: ({}) => {return_type}",
                     route,
                     parameter_types
                         .into_iter()
@@ -61,9 +73,10 @@ impl Server {
                         .join(", ")
                 );
 
-                println!("{signature}");
                 signature
             })
-            .collect()
+            .collect::<Vec<_>>();
+
+        format!("{{ {} }}", body.join(", "))
     }
 }
