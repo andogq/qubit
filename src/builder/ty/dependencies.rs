f9dd296e3ac688ec23de9a0053fbd54469e10ed9
Tom Anderson <tom@ando.gq>
2024-05-23 10:38:43 +1000

    properly generate `TypeDependencies` trait for built-in generic types

--- src/builder/ty/dependencies.rs
+++ src/builder/ty/dependencies.rs
@@ -7,11 +7,45 @@ pub trait TypeDependencies {
 
 macro_rules! impl_type_dependencies {
     ($($t:ident<$($generic:ident),*>),*) => {
-        $(impl<$($generic),*> TypeDependencies for $t<$($generic),*> {})*
+        $(impl<$($generic),*> TypeDependencies for $t<$($generic),*>
+            where $($generic: ts_rs::TS + crate::TypeDependencies),*
+            {
+            fn get_deps(dependencies: &mut BTreeMap<String, String>) {
+                $(impl_type_dependencies!(generic: $generic, dependencies);)*
+            }
+        })*
     };
 
     ($($t:ty),*) => {
         $(impl TypeDependencies for $t {})*
+    };
+
+    (tuple: $t:ident) => {
+        impl<$t> TypeDependencies for ($t,)
+            where $t: ts_rs::TS + crate::TypeDependencies,
+        {
+            fn get_deps(dependencies: &mut BTreeMap<String, String>) {
+                impl_type_dependencies!(generic: $t, dependencies);
+            }
+        }
+    };
+
+    (tuple: $t:ident $(, $t_other:ident)*) => {
+        impl<$t, $($t_other),*> TypeDependencies for ($t, $($t_other),*)
+            where $t: ts_rs::TS + crate::TypeDependencies,
+            $($t_other: ts_rs::TS + crate::TypeDependencies),*
+        {
+            fn get_deps(dependencies: &mut BTreeMap<String, String>) {
+                impl_type_dependencies!(generic: $t, dependencies);
+                $(impl_type_dependencies!(generic: $t_other, dependencies);)*
+            }
+        }
+
+        impl_type_dependencies!(tuple: $($t_other),*);
+    };
+
+    (generic: $generic:ident, $dependencies:ident) => {
+        <$generic as TypeDependencies>::get_deps($dependencies)
     };
 }
 
@@ -36,21 +70,9 @@ impl_type_dependencies!(
 );
 impl_type_dependencies!(
     Vec<T>,
+    Box<T>,
     Option<T>,
     Result<T, E>,
     HashMap<K, V>
 );
-
-macro_rules! impl_tuples {
-    ($t:ident) => {
-        impl<$t> TypeDependencies for ($t,) {}
-    };
-
-    ($t:ident $(, $t_other:ident)*) => {
-        impl<$t, $($t_other),*> TypeDependencies for ($t, $($t_other),*) {}
-
-        impl_tuples!($($t_other),*);
-    };
-}
-
-impl_tuples!(T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
+impl_type_dependencies!(tuple: T0, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
