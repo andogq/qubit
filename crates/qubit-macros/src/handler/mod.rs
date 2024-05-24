e0763af5196f162a552388a067c7c88d37ed2373
Tom Anderson <tom@ando.gq>
2024-05-24 23:51:50 +1000

    break `HandlerOptions` into own file

--- crates/qubit-macros/src/handler/mod.rs
+++ crates/qubit-macros/src/handler/mod.rs
@@ -1,53 +1,10 @@
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::{quote, ToTokens};
-use syn::{
-    meta::ParseNestedMeta, spanned::Spanned, Error, FnArg, ItemFn, LitStr, Pat, Result, ReturnType,
-    Type, TypeImplTrait,
-};
+use syn::{spanned::Spanned, Error, FnArg, ItemFn, Pat, Result, ReturnType, Type, TypeImplTrait};
 
-/// Handlers can have different variations depending on how they interact with the client.
-pub enum HandlerKind {
-    /// Query handlers support the standard request/response pattern.
-    Query,
+mod options;
 
-    /// Subscriptions have an initial request, and returns a stream of responses that the client
-    /// will continue to consume.
-    Subscription,
-}
-
-/// Options that may be attached to a handler.
-#[derive(Default)]
-pub struct HandlerOptions {
-    /// The kind of handler.
-    pub kind: Option<HandlerKind>,
-
-    /// Overridden name for the handler.
-    pub name: Option<Ident>,
-}
-
-impl HandlerOptions {
-    /// Attempt to parse the handler kind from [`ParseNestedMeta`].
-    pub fn parse(&mut self, meta: ParseNestedMeta) -> Result<()> {
-        if meta.path.is_ident("query") {
-            self.kind = Some(HandlerKind::Query);
-            Ok(())
-        } else if meta.path.is_ident("subscription") {
-            self.kind = Some(HandlerKind::Subscription);
-            Ok(())
-        } else if meta.path.is_ident("name") {
-            // Extract name from the attribute
-            let name = meta.value()?.parse::<LitStr>()?.value();
-
-            // Create the ident for the handler name
-            let ident = Ident::new(&name, meta.input.span());
-
-            self.name = Some(ident);
-            Ok(())
-        } else {
-            Err(meta.error("unsupported handler property"))
-        }
-    }
-}
+pub use options::*;
 
 /// Generates the implementation for [`qubit::Handler`] for the provided handler function. The
 /// [`HandlerKind`] is required alter how the handler is applied to the router. This could be
