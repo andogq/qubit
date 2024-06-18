7dab462f3e57a9998d728fe68edb2561532df1ba
Tom Anderson <tom@ando.gq>
2024-06-18 22:05:43 +1000

    add logging back in

--- scripts/generate-lock-files.sh
+++ scripts/generate-lock-files.sh
@@ -4,5 +4,7 @@ MANIFESTS=$(find . -name Cargo.toml ! -path './target/*')
 
 for MANIFEST in $MANIFESTS
 do
+    echo "Updating Cargo.lock for $MANIFEST"
+
     cargo generate-lockfile --manifest-path="$MANIFEST"
 done
