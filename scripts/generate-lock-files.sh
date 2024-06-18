379e77b1c79aea93c16f1c25490d5a39aa598adf
Tom Anderson <tom@ando.gq>
2024-06-18 21:49:44 +1000

    don't log out inscript

--- scripts/generate-lock-files.sh
+++ scripts/generate-lock-files.sh
@@ -4,7 +4,5 @@ MANIFESTS=$(find . -name Cargo.toml ! -path './target/*')
 
 for MANIFEST in $MANIFESTS
 do
-    echo "Updating Cargo.lock for $MANIFEST"
-
     cargo generate-lockfile --manifest-path="$MANIFEST"
 done
