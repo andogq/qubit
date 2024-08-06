03b379de5167cf5ef29946458437c5ee8255e128
Lucas Ávila <lucaxx@gmail.com>
2024-08-06 19:02:26 -0300

    Upgrade ts_rs

--- src/builder/ty/util/exporter.rs
+++ src/builder/ty/util/exporter.rs
@@ -1,9 +1,6 @@
 use std::path::{Path, PathBuf};
 
-use ts_rs::{
-    typelist::{TypeList, TypeVisitor},
-    Dependency, ExportError, TS,
-};
+use ts_rs::{Dependency, ExportError, TypeVisitor, TS};
 
 /// Visitor for [`ts_rs::TypeList`], which will export each type and any dependent types. It will
 /// also save a list of top level [`Dependency`]s that must be imported in order for the original
@@ -15,10 +12,10 @@ struct TypeListExporter {
 
 impl TypeListExporter {
     /// Create a new empty instance.
-    pub fn new(out_dir: impl AsRef<Path>) -> Self {
+    pub fn new(out_dir: impl AsRef<Path>, dependencies: Vec<Dependency>) -> Self {
         Self {
             out_dir: out_dir.as_ref().to_owned(),
-            dependencies: Vec::new(),
+            dependencies,
         }
     }
 
@@ -26,23 +23,12 @@ impl TypeListExporter {
     pub fn into_inner(self) -> Vec<Dependency> {
         self.dependencies
     }
-
-    /// Export the provided type list, and return the dependency list.
-    pub fn export(type_list: impl TypeList, out_dir: impl AsRef<Path>) -> Vec<Dependency> {
-        let mut visitor = Self::new(out_dir);
-
-        type_list.for_each(&mut visitor);
-
-        visitor.into_inner()
-    }
 }
 
 impl TypeVisitor for TypeListExporter {
     fn visit<T: TS + 'static + ?Sized>(&mut self) {
         let Some(dep) = Dependency::from_ty::<T>() else {
             // Type must be a primitive, so recurse to ensure all generics are properly exported.
-            T::dependency_types().for_each(self);
-            T::generics().for_each(self);
 
             return;
         };
@@ -67,8 +53,10 @@ pub fn export_with_dependencies<T: 'static + TS>(
     out_dir: impl AsRef<Path>,
 ) -> Result<Vec<Dependency>, ExportError> {
     // Ensure any generics used in the type are exported
-    let mut dependencies =
-        TypeListExporter::export(T::dependency_types().extend(T::generics()), &out_dir);
+    let mut exporter = TypeListExporter::new(&out_dir, T::dependencies());
+    T::visit_generics(&mut exporter);
+
+    let mut dependencies = exporter.into_inner();
 
     // Make sure the top-level type isn't a primitive, so it can be exported.
     if T::output_path().is_some() {
