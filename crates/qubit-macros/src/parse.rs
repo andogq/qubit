0ac05a82b0c52d19f6aa8c592326b53a096ee1f6
Tom Anderson <tom@ando.sh>
2025-06-27 17:20:25 +1000

    fix: better span for unknown attributes

--- crates/qubit-macros/src/parse.rs
+++ crates/qubit-macros/src/parse.rs
@@ -138,7 +138,7 @@ impl AttributesBuilder {
             return Ok(());
         }
 
-        Err(AttributesParseError::UnsupportedProperty(meta.input.span()))
+        Err(AttributesParseError::UnsupportedProperty(meta.path.span()))
     }
 }
 
@@ -154,7 +154,7 @@ pub enum AttributesParseError {
     KindProvided(Span),
     #[error("handler name has already been provided")]
     NameProvided(Span),
-    #[error("unsupported handler property")]
+    #[error("unknown attribute")]
     UnsupportedProperty(Span),
     #[error(transparent)]
     ParseError(#[from] Error),
---
+++ crates/qubit-macros/tests/ui/attribute-unknown.rs
@@ -0,0 +1,6 @@
+use qubit_macros::handler;
+
+#[handler(query, random_attribute)]
+async fn my_handler() {}
+
+fn main() {}
