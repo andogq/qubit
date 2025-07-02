66e96c7b84d11537711855af503d0ea115058660
Tom Anderson <tom@ando.sh>
2025-07-02 20:33:12 +1000

    fix: better generated parameter TS types

--- crates/qubit-macros/src/codegen.rs
+++ crates/qubit-macros/src/codegen.rs
@@ -106,7 +106,7 @@ fn generate_ts_signature(
     let handler_ty = handler_kind_str.as_ref();
     let params = params
         .map(|parse_params| {
-            let fmt_str = "{}: {}, ".repeat(parse_params.len());
+            let fmt_str = vec!["{}: {}"; parse_params.len()].join(", ");
             let fmt_params = parse_params.iter().map(|(ident, ty)| {
                 let ident = ident.to_string();
                 quote! { #ident, <#ty as ::qubit::__private::ts_rs::TS>::name() }
