12af0c3dbe67aa8be2dba2f509244238fe76766a
Tom Anderson <tom@ando.sh>
2025-07-23 21:56:53 +1000

    remove un-needed tests

--- crates/qubit-macros/tests/ui/handler-output-non-subscription-stream.rs
+++
@@ -1,6 +0,0 @@
-use qubit_macros::handler;
-
-#[handler(query)]
-async fn my_handler() -> impl Stream<Item = u32> {}
-
-fn main() {}
--- crates/qubit-macros/tests/ui/handler-output-subscription-no-output.rs
+++
@@ -1,6 +0,0 @@
-use qubit_macros::handler;
-
-#[handler(subscription)]
-async fn my_handler() {}
-
-fn main() {}
